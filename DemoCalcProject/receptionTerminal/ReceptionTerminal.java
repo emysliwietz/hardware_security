029671cf2ebcfee196e6e77eca24d86ecd485825
MaJoEi <matthias.eisenlohr@web.de>
2021-04-12 14:16:27 +0200

    Implemented P4 on terminal side

--- DemoCalcProject/receptionTerminal/ReceptionTerminal.java
+++ DemoCalcProject/receptionTerminal/ReceptionTerminal.java
@@ -16,6 +16,9 @@ public class ReceptionTerminal implements Receivable, Communicator {
 
     private ReceptionTerminal.RTCrypto rtc;
     public PublicKey dbPubSK;
+    boolean cardAuthenticated = false;
+    UUID termNonce; //Placeholder
+    UUID scNonce; //Placeholder
 
 
     @Override
@@ -31,7 +34,41 @@ public class ReceptionTerminal implements Receivable, Communicator {
         rtc = new receptionTerminal.ReceptionTerminal.RTCrypto(rtID, rtCertificate);
     }
 
-
+    public void carReturn(Smartcard sc, PublicKey scPubSK){
+        if (!cardAuthenticated){
+            return; //TODO: Placeholder
+        }
+        byte[] msg1b = waitForInput();
+        Object[] msg1o = processMessage(msg1b);
+        int seqNum = (int) msg1o[1];
+        //TODO: Check sequence number
+        boolean manipulation = (boolean) msg1o[2];
+        byte[] msg1Hash = rtc.unsign(((byte[]) msg1o[3]), scPubSK);
+        byte[] msg1ConfHash = rtc.createHash(prepareMessage(((byte) msg1o[0]), seqNum, manipulation));
+        if(msg1Hash != msg1ConfHash){
+            //TODO: Error
+        }
+        if (manipulation){
+            //TODO: Throw exception/error
+        }
+        UUID kmmNonce = rtc.generateNonce();
+        int seqNum2 = 0; //Placeholder
+        byte[] msg2Sign = rtc.hashAndSign(prepareMessage(kmmNonce, seqNum2));
+        send(sc, kmmNonce, seqNum2, msg2Sign);
+        byte[] msg3b = waitForInput();
+        Object[] msg3 = processMessage(msg3b);
+        int kilometerage = (int) msg3[0];
+        UUID kmmNonceResp = (UUID) msg3[1];
+        if(kmmNonce != kmmNonceResp){
+            //TODO: Error
+        }
+        int seqNum3 = (int) msg3[2];
+        byte[] msg3Hash = rtc.unsign(((byte[]) msg3[3]),scPubSK);
+        byte[] validMsg3Hash = rtc.createHash(prepareMessage(kilometerage, kmmNonceResp, seqNum3));
+        if(msg3Hash != validMsg3Hash){
+            //TODO: Error
+        }
+    }
 
     private static class RTCrypto extends CryptoImplementation {
 
