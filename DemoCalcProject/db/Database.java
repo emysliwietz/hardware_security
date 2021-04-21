f62ef433acd22dbcc728bec1e8f578ea361349d3
Alessandra <46654416+AVanVeen@users.noreply.github.com>
2021-04-21 15:01:46 +0200

    Update Database.java
    Very basic DB stuff

--- DemoCalcProject/db/Database.java
+++ DemoCalcProject/db/Database.java
@@ -10,6 +10,11 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Database extends CryptoImplementation implements Communicator {
 
@@ -40,6 +45,17 @@ public class Database extends CryptoImplementation implements Communicator {
     }
 
     public Database(){
+        Connection c = null;
+
+        //source for now: https://www.tutorialspoint.com/sqlite/sqlite_java.htm
+        //So I know how to find the tutorial again
+        try{
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db"); //doesnt work yet but so we have the thing
+        } catch(Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
 
     }
 
@@ -54,11 +70,12 @@ public class Database extends CryptoImplementation implements Communicator {
             return;
         }
         Object[] responseData = processMessage(response);
-        byte[] cardID = (byte[]) responseData[0];
+        byte[] cardID = (byte[]) responseData[0]; //Get card ID so DB knows which car is assigned to which card
 
-        //pretend it stores cardID and autoID
+        //Some function so it stores the link between an autoID and a cardID
 
         //just a fake generate for now so it wont throw an error
+        //Should be retrieved from the database
         Object [] dbKeyPair = generateKeyPair();
         PublicKey dbPubSK = (PublicKey) dbKeyPair[0];
         PrivateKey dbPrivSK = (PrivateKey) dbKeyPair[1];
@@ -71,7 +88,7 @@ public class Database extends CryptoImplementation implements Communicator {
         send(reception, message);
     }
 
-    void generateStuff() { //TODO: Put those variables in global object array/mini-database
+ /*   void generateStuff() { //TODO: Put those variables in global object array/mini-database
         Object [] dbKeyPair = generateKeyPair();
         PublicKey dbPubSK = (PublicKey) dbKeyPair[0];
         PrivateKey dbPrivSK = (PrivateKey) dbKeyPair[1];
@@ -90,8 +107,38 @@ public class Database extends CryptoImplementation implements Communicator {
         byte[] scCERT = issueCertificate(scPubSK, scID, dbPrivSK);
         byte[] autoCERT = issueCertificate(autoPubSK, autoID, dbPrivSK);
         byte[] rtCERT = issueCertificate(rtPubSK, rtID, dbPrivSK);
+    }*/
+
+    void generateCard(){
+        Object [] scKeyPair = generateKeyPair();
+        PublicKey scPubSK = (PublicKey) scKeyPair[0];
+        PrivateKey scPrivSK = (PrivateKey) scKeyPair[1];
+        byte[] scID = UUID.randomUUID().toString().getBytes();
+        byte[] scCERT = issueCertificate(scPubSK, scID, dbPrivSK);
+
+        // actually store in DB, not the private key though
+        // and send the info back
+    }
+    void generateAuto(){
+        Object [] autoKeyPair = generateKeyPair();
+        PublicKey autoPubSK = (PublicKey) autoKeyPair[0];
+        PrivateKey autoPrivSK = (PrivateKey) autoKeyPair[1];
+        byte[] autoID = UUID.randomUUID().toString().getBytes();
+        byte[] autoCERT = issueCertificate(autoPubSK, autoID, dbPrivSK);
+
+        // actually store in DB, not the private key though
+        // and send the info back
     }
 
+    void generateTerminal(){
+        Object [] rtKeyPair = generateKeyPair();
+        PublicKey rtPubSK = (PublicKey) rtKeyPair[0];
+        PrivateKey rtPrivSK = (PrivateKey) rtKeyPair[1];
+        byte[] rtID = UUID.randomUUID().toString().getBytes();
+        byte[] rtCERT = issueCertificate(rtPubSK, rtID, dbPrivSK);
 
+        // actually store in DB, not the private key though
+        // and send the info back
 
+    }
 }
